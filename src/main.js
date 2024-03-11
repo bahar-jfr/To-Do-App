1200920da7d3f4bc1b29fd168e095c7cb5278520
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-03-11 12:42:05 +0330

    feat: add event on submit button and get user Info

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@
 const plusIcon = document.getElementById("plus_icon");
 const modal = document.getElementById("modal");
 const overlay = document.getElementById("overlay");
+const submitBtn = document.getElementById("submit_btn");
 
 // Events
+submitBtn.addEventListener("click", addNewTask);
+
+function addNewTask() {
+  const taskName = document.getElementById("task_name").value;
+  const taskPriority = document.getElementById("task_priority").value;
+  const taskStatus = document.getElementById("task_status").value;
+}
 
 // Open modal
 plusIcon.addEventListener("click", openModal);
