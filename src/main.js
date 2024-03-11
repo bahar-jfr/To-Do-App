c8966dd218ebcdb0fb2322e462659be26eeca8ae
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-03-11 16:07:16 +0330

    feat: append tr and td to tbody

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 const allInfoArray = [];
+const userObjectKeys = ["name", "priority", "status", "deadline", "action"];
 
 // Get Elements
 const plusIcon = document.getElementById("plus_icon");
 const modal = document.getElementById("modal");
 const overlay = document.getElementById("overlay");
 const submitBtn = document.getElementById("submit_btn");
+const tabelBody = document.getElementById("table_body");
 
 // Events
 submitBtn.addEventListener("click", addNewTask);
@@ -25,26 +27,42 @@ function closeModal() {
 
 // Add new task's data to localstorage
 function addNewTask() {
-    const taskName = document.getElementById("task_name");
-    const taskPriority = document.getElementById("task_priority").value;
-    const taskStatus = document.getElementById("task_status").value;
-  
-    const userObject = {
-      id: Date.now(),
-      name: taskName.value,
-      priority: taskPriority,
-      status: taskStatus,
-    };
-  
-    allInfoArray.push(userObject);
-  
-    localStorage.setItem("task", JSON.stringify(allInfoArray));
-  
-    taskName.value = "";
-  }
+  const taskName = document.getElementById("task_name");
+  const taskPriority = document.getElementById("task_priority").value;
+  const taskStatus = document.getElementById("task_status").value;
+  tabelBody.innerHTML = ""
+
+  const userObject = {
+    id: Date.now(),
+    name: taskName.value,
+    priority: taskPriority,
+    status: taskStatus,
+  };
+
+  allInfoArray.push(userObject);
+
+  localStorage.setItem("task", JSON.stringify(allInfoArray));
+
+  taskName.value = "";
 
   const tasks = JSON.parse(localStorage.getItem("task"));
-  tasks.forEach(item => {
-    
+  tasks.forEach((item) => {
+    const row = document.createElement("tr");
+
+    userObjectKeys.forEach((key) => {
+      const cell = document.createElement("td");
+      if (key === "deadline") {
+        cell.innerText = "date";
+      } else if (key === "action") {
+        cell.innerText = "action";
+      } else {
+        cell.innerText = item[key];
+      }
+      row.append(cell);
+      console.log(key);
+    });
+
+    tabelBody.append(row);
   });
-  
+
+}
